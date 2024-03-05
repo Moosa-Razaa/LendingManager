5b8167305b7969a3b32f54986dcf4acedcdba2fd
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-03-06 00:47:30 +0100

    login form inprocess

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  OnSubmitLogin() 
+  {
+    console.log(`Username: ${this.username}, Password: ${this.password}`);
+  }
+}
